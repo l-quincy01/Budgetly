1ddddcb56d59d5bcffb000da8d64f8c6d6221b0c
Lesego Pitsi <l.quincypitsi@gmail.com>
2024-11-06 17:15:07 +0200

    Intial Commit

---
+++ Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+	public class Category
+	{
+        [Key]
+		public int CategoryID { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public int Icon { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public int Type { get; set; }
+
+    }
+}
+
