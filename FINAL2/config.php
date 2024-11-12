81836a6a70df4a57350daf911e71ebf765cebe6c
Lesego Pitsi <l.quincypitsi@gmail.com>
2024-11-12 23:02:10 +0200

    Project clean up

--- FINAL2/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-define("SERVERNAME","IS3-DEV.ICT.RU.AC.ZA");
-define("USERNAME","TheCreatorsCollective");
-define("PASSWORD","TheCreatorsCollective");
-define("DATABASE","TheCreatorsCollective");
-
-?>
